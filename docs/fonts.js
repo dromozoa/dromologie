9daa8a52d6f317e18e2bcbf5c3bb52527cf89691
Tomoyuki Fujimori <moyu@dromozoa.com>
2024-03-18 07:44:38 +0900

    ルビの有無切り替え。

--- docs/fonts.js
+++ docs/fonts.js
@@ -37,6 +37,32 @@ document.addEventListener("DOMContentLoaded", () => {
       document.querySelector(".main").classList.remove("width-24em");
     }
   });
+
+  document.querySelector("#ruby").addEventListener("change", ev => {
+    if (ev.target.checked) {
+      document.querySelectorAll(".ruby ruby").forEach(node => node.classList.remove("hide"));
+      document.querySelectorAll(".ruby .ruby-base").forEach(node => node.classList.add("hide"));
+    } else {
+      document.querySelectorAll(".ruby ruby").forEach(node => node.classList.add("hide"));
+      document.querySelectorAll(".ruby .ruby-base").forEach(node => node.classList.remove("hide"));
+    }
+  });
+
+  document.querySelectorAll("ruby").forEach(node1 => {
+    let base = "";
+    node1.querySelectorAll("rb").forEach(node => base += node.innerText);
+
+    const node2 = document.createElement("span");
+    node2.classList.add("ruby");
+
+    const node3 = document.createElement("span");
+    node3.classList.add("ruby-base");
+    node3.classList.add("hide");
+    node3.innerText = base;
+
+    node1.replaceWith(node2);
+    node2.append(node1, node3);
+  });
 });
 
 //-------------------------------------------------------------------------
