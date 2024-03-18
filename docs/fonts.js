aa2992dea2b1358b9febccc7e92f82b652d72bb3
Tomoyuki Fujimori <moyu@dromozoa.com>
2024-03-18 09:34:29 +0900

    Lorem Ipsumを追加。

--- docs/fonts.js
+++ docs/fonts.js
@@ -16,17 +16,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelector("#font-weight-bold").addEventListener("change", ev => {
     if (ev.target.checked) {
-      document.querySelector(".melos").classList.add("font-weight-bold");
+      document.querySelector(".text").classList.add("font-weight-bold");
     } else {
-      document.querySelector(".melos").classList.remove("font-weight-bold");
+      document.querySelector(".text").classList.remove("font-weight-bold");
     }
   });
 
   document.querySelector("#text-align-justify").addEventListener("change", ev => {
     if (ev.target.checked) {
-      document.querySelector(".melos").classList.add("text-align-justify");
+      document.querySelector(".text").classList.add("text-align-justify");
     } else {
-      document.querySelector(".melos").classList.remove("text-align-justify");
+      document.querySelector(".text").classList.remove("text-align-justify");
     }
   });
 
