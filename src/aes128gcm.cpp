f2ac5ad947e5892d74e1e302cab037d48a2c129e
Tomoyuki Fujimori <moyu@dromozoa.com>
2024-03-20 01:20:57 +0900

    mbedtlsを使ってaes128gcmを実装。

--- src/aes128gcm.cpp
+++ src/aes128gcm.cpp
@@ -1,21 +1,16 @@
 #include <mbedtls/build_info.h>
 #include <mbedtls/cipher.h>
 #include <mbedtls/error.h>
+#include <mbedtls/gcm.h>
 
+#include <algorithm>
 #include <cstddef>
-#include <cstdint>
 #include <iostream>
+#include <iterator>
 #include <memory>
 #include <stdexcept>
 #include <string>
 #include <vector>
-
-// key
-// nonce
-// salt
-// plain text
-
-
 
 std::vector<unsigned char> decode_hex(const std::string& source) {
   std::vector<unsigned char> result;
@@ -36,20 +31,37 @@ void check(int result) {
 int main(int argc, char* argv[]) {
   try {
     if (argc < 3) {
-      std::cerr << argv[0] << " hex-key hex-iv\n";
+      std::cerr << argv[0] << " hex-key hex-iv <input >output\n";
       return 1;
     }
 
     const auto key = decode_hex(argv[1]);
     const auto iv = decode_hex(argv[2]);
 
-    mbedtls_cipher_context_t context;
-    std::unique_ptr<mbedtls_cipher_context_t, decltype(&mbedtls_cipher_free)> ctx(&context, mbedtls_cipher_free);
-    mbedtls_cipher_init(ctx.get());
+    std::vector<unsigned char> input;
+    std::copy(std::istreambuf_iterator<char>(std::cin), std::istreambuf_iterator<char>(), std::back_inserter(input));
 
-    check(mbedtls_cipher_setup(ctx.get(), mbedtls_cipher_info_from_type(MBEDTLS_CIPHER_AES_128_GCM)));
-    check(mbedtls_cipher_setkey(ctx.get(), &key[0], key.size() * 8, MBEDTLS_ENCRYPT));
-    check(mbedtls_cipher_set_iv(ctx.get(), &iv[0], iv.size() * 8));
+    if (input.size() > 3993) {
+      std::cerr << "input too large\n";
+      return 1;
+    }
+
+    std::vector<unsigned char> output(4096);
+    std::size_t output_size1 = 0;
+    std::size_t output_size2 = 0;
+    std::vector<unsigned char> tag(16);
+
+    mbedtls_gcm_context context;
+    std::unique_ptr<mbedtls_gcm_context, decltype(&mbedtls_gcm_free)> ctx(&context, mbedtls_gcm_free);
+    mbedtls_gcm_init(ctx.get());
+
+    check(mbedtls_gcm_setkey(ctx.get(), MBEDTLS_CIPHER_ID_AES, &key[0], key.size() * 8));
+    check(mbedtls_gcm_starts(ctx.get(), MBEDTLS_GCM_ENCRYPT, &iv[0], iv.size()));
+    check(mbedtls_gcm_update(ctx.get(), &input[0], input.size(), &output[0], output.size(), &output_size1));
+    check(mbedtls_gcm_finish(ctx.get(), &output[output_size1], output.size() - output_size1, &output_size2, &tag[0], tag.size()));
+
+    std::cout.write(reinterpret_cast<const char*>(&output[0]), output_size1 + output_size2);
+    std::cout.write(reinterpret_cast<const char*>(&tag[0]), tag.size());
 
   } catch (const std::exception& e) {
     std::cerr << "caught exception: " << e.what() << "\n";
